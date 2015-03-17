3945ee211cb8d28f45284673a21c01e3b156c958
mapler <maplerme@gmail.com>
2015-03-17 15:48:48 +0900

    Translate Chinese

--- app/src/main/java/cn/socialclock/manager/SnsManager.java
+++ app/src/main/java/cn/socialclock/manager/SnsManager.java
@@ -17,23 +17,35 @@ public class SnsManager {
         long snoozePeriodInSeconds = (alarmEvent.getEndAt().getTimeInMillis() -
                 alarmEvent.getStartAt().getTimeInMillis()) / 1000;
 
-        String snoozePeriodMessage = ""
-                + (snoozePeriodInSeconds / 60 > 0 ? (snoozePeriodInSeconds / 60 + "分") : "")
-                + snoozePeriodInSeconds % 60 + "秒";
-
-        String snsMessage = "(这是测试)"
-                + "我本来定的"
-                + alarmEvent.getStartAt().get(Calendar.HOUR)
-                + "点"
-                + String.format("%02d", alarmEvent.getStartAt().get(Calendar.MINUTE))
-                + "分的闹钟，结果"
-                + alarmEvent.getEndAt().get(Calendar.HOUR)
-                + "点"
-                + String.format("%02d", alarmEvent.getEndAt().get(Calendar.MINUTE))
-                + "分才起来。按掉"
-                + alarmEvent.getSnoozeTimes() + "次闹铃，赖床" + snoozePeriodMessage
-                + "...我果然是个废物。(" + DatetimeFormatter.calendarToString(alarmEvent.getEndAt())
-                + ")";
+        String snsMessage;
+        if (snoozePeriodInSeconds > 60 || alarmEvent.getSnoozeTimes() > 0 ) {
+            String snoozePeriodMessage = ""
+                    + (snoozePeriodInSeconds / 60 > 0 ? (snoozePeriodInSeconds / 60 + " min") :
+                    snoozePeriodInSeconds % 60 + " sec");
+            snsMessage = "(test) "
+                    + "Alarm at "
+                    + alarmEvent.getStartAt().get(Calendar.HOUR)
+                    + ":"
+                    + String.format("%02d", alarmEvent.getStartAt().get(Calendar.MINUTE))
+                    + ". Get up at "
+                    + alarmEvent.getEndAt().get(Calendar.HOUR)
+                    + ":"
+                    + String.format("%02d", alarmEvent.getEndAt().get(Calendar.MINUTE))
+                    + ". Snooze "
+                    + alarmEvent.getSnoozeTimes() + (alarmEvent.getSnoozeTimes() > 1 ? " times. " : " time. ")
+                    + "Late " + snoozePeriodMessage
+                    + " (" + DatetimeFormatter.calendarToString(alarmEvent.getEndAt())
+                    + ")";
+        }
+        else {
+            snsMessage = "(test) "
+                    + "Alarm and get up at "
+                    + alarmEvent.getStartAt().get(Calendar.HOUR)
+                    + ":"
+                    + String.format("%02d", alarmEvent.getStartAt().get(Calendar.MINUTE))
+                    + ". (" + DatetimeFormatter.calendarToString(alarmEvent.getEndAt())
+                    + ")";
+        }
 
         return snsMessage;
     }
